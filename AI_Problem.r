c93ad01d3836094a6623c303638239315312be30
Mohamad Badiezadegan <mbzadegan@gmail.com>
2025-04-09 18:57:01 -0700

    Update AI_Problem.r

--- AI_Problem.r
+++ AI_Problem.r
@@ -1,6 +1,6 @@
 # Here's an example of R code tackling a challenging AI problem: "Training a neural network to classify handwritten digits using the MNIST dataset." 
 # This problem is a classic in AI and machine learning.
-# We'll use the keras package in R, which provides an interface to TensorFlow, a powerful AI framework.
+# We will use the keras package in R, which provides an interface to TensorFlow, a powerful AI framework.
 # Install TensorFlow and Keras for R please run:
 # install_keras()
 
