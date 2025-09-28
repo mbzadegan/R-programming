333cdfebb349675d62f38515bb46b8ef1beed290
Mohamad Badiezadegan <mbzadegan@gmail.com>
2025-09-28 00:18:04 -0700

    Update BlumBlumShub.r

--- BlumBlumShub.r
+++ BlumBlumShub.r
@@ -1,4 +1,4 @@
-# Function to generate n random numbers using Blum Blum Shub
+# Function to generate n random numbers using Blum Blum Shub.
 
 blum_blum_shub <- function(n, p, q, seed) {
   # Ensure p and q are congruent to 3 mod 4
